9beba10c6ef31b2a9c56a3b614667e3545ab2a12
Sanjay-128k <sanjay1283024@gmail.com>
2022-07-17 23:11:46 +0530

    2 files modified

--- fs/streams.js
+++ fs/streams.js
@@ -1,13 +1,19 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('./files/streamfile.txt');
+const readStream = fs.createReadStream('./files/streamfile.txt', {
+  encoding: 'utf8',
+});
 const writeStream = fs.createWriteStream('./files/streamfile1.txt');
+
+// we can pass encoding property or either we can use .toString() method inside the stream function
+/*
+
 let count = 0;
 
 readStream.on('data', (chunk) => {
   count++;
   console.log('.............NEW CHUNK..........');
-  console.log(chunk.toString());
+  console.log(chunk);
   writeStream.write(
     '.............................NEW CHUNK.........................................................'
   );
@@ -17,3 +23,10 @@ readStream.on('data', (chunk) => {
 readStream.on('end', () => {
   console.log(count);
 });
+*/
+
+/*piping
+
+readStream.pipe(writeStream);
+
+*/
