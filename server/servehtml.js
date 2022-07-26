ddc6a523c96d1081287db3f70d19dde890846666
SanjayAlagappan <sanjay1283024@gmail.com>
2022-07-26 14:49:35 +0530

    1 file modified

--- server/servehtml.js
+++ server/servehtml.js
@@ -16,6 +16,11 @@ const server = http.createServer((req, res) => {
       res.statusCode = 200;
       break;
 
+    case '/about-me':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      break;
+
     default:
       path += '/404.html';
       res.statusCode = 404;
